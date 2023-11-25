2ab7296114ce27023ad43e9af93281a43e932291
LuisHM5 <a338926@uach.mx>
2023-11-24 20:27:51 -0600

    Final changes

--- Runner_Admin_Server_dotnet/Runner_Admin_Checkpoints/Runner_Admin_Checkpoints/Models/RegistroViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Runner_Admin_Checkpoints.Models
-{
-    public class RegistroViewModel
-    {
-        public int ID { get; set; }
-        public string NombreTarjeta { get; set; }
-        public string Tiempo { get; set; }
-    }
-}
